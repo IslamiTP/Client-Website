02e6b2fafe069700ef4ef2add37690f35f95bc96
Penzias <147297145+IslamiTP@users.noreply.github.com>
2025-08-13 19:08:13 -0400

    initialization of home page

---
+++ bojo/src/app/home/page.tsx
@@ -0,0 +1,27 @@
+import image from "next/image";
+
+
+export default function Home() {
+    return(
+        <div className ="">
+        
+
+
+        {/*  ==============================  Home Page Footer  ==============================   */}
+        <footer>
+            <a
+                className=""
+                href = ""
+                target ="_blank"
+                rel ="noopener noreferrer"
+            
+            >
+
+
+            </a>
+
+        </footer>
+        
+        </div>
+    );
+}
--- bojo/src/app/page.tsx
+++ bojo/src/app/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
 import Image from "next/image";
 
 export default function Home() {
