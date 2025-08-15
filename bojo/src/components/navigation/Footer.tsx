fc0066d77eac97aa350327f3a4792e5d0e2fd6b0
Penzias <147297145+IslamiTP@users.noreply.github.com>
2025-08-14 22:15:38 -0400

    Changes to the footer

--- bojo/src/components/navigation/Footer.tsx
+++ bojo/src/components/navigation/Footer.tsx
@@ -7,18 +7,30 @@ export default function FooterBar() {
 
   return (
     <footer className = "bg-white text-white" p-10>
-      <div className="">
+      <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+        
+        {/* Bojo Resort Logo Placement */}
         <div>
-          <Image 
-            
-            src="/bojo.svg"
-            alt="Bojo Logo"
-            width={150}
-            height={50}
-            >
-
-          </Image>
+          <Image src="/bojo.svg" alt="Bojo Logo" width={175} height={50} className="mb-2"/>
+          <p className='text-black'>Bojo Resort - Circa 1994 </p>
         </div>
+        
+        <div>
+          <Link href="/kontakt">
+            <h3 className="font-bold text-black mb-1 cursor-pointer hover:underline"> Kontakt </h3>
+          </Link>
+          <ul>
+            <li>
+              <span className="hover:underline text-black"> +355 69 20 79 023 </span>
+            </li>
+            <li>
+              <Link href="/kontakt">
+                <h3 className="text-black mb-1 cursor-pointer hover:underline">  info@bojoresort.al </h3>
+              </Link>
+            </li>
+          </ul>
+        </div>
+
 
       </div>
     
