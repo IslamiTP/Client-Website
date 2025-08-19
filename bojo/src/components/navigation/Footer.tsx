0d6c3a95a5e4a22122f0c3445760e8b18bab5033
IslamiTP <147297145+IslamiTP@users.noreply.github.com>
2025-08-19 19:07:20 -0400

    New implementations
    implemented new additions to the footer.
    
    Completed 2 rows, newsletter is setup for api.

--- bojo/src/components/navigation/Footer.tsx
+++ bojo/src/components/navigation/Footer.tsx
@@ -7,48 +7,129 @@ export default function FooterBar() {
 
   return (
     <footer className = "bg-white text-white p-10">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+      <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         
         {/* Bojo Resort Logo Placement */}
         <div>
           <Image src="/bojo.svg" alt="Bojo Logo" width={175} height={50} className="mb-2"/>
-          <p className='text-black'>Bojo Resort - Circa 1994 </p>
+          <p className='text-black'> Bojo Resort - Circa 1994 </p>
         </div>
         
+        {/* Kontak Information */}
         <div>
           <Link href="/kontakt">
             <h3 className="font-bold text-black mb-1 cursor-pointer hover:underline"> Kontakt </h3>
           </Link>
           <ul>
+
+            {/* Phone Number */}
             <li>
-              <span className="hover:underline text-black"> +355 69 20 79 023 </span>
+              <span className="hover:underline text-black"> 
+                +355 69 20 79 023 
+              </span>
             </li>
+
+            {/* Email */}
             <li>
               <Link href="/kontakt">
-                <h3 className="text-black mb-1 cursor-pointer hover:underline">  info@bojoresort.al </h3>
+                <h3 className="text-black mb-1 cursor-pointer hover:underline">  
+                  info@bojoresort.al 
+                </h3>
               </Link>
             </li>
+
+            {/* Address */}
+            <li>
+              <h3 className="text-black mb-1 cursor-pointer hover:underline"> 
+                Bojo Resort<br></br>
+                Orikum, Albania
+              </h3>
+            </li>
+
+            {/* Social Links */}
+            <ul className="flex space-x-1">              
+              
+              {/* Instagram */}
+              <li>                
+                <Link href="https://www.instagram.com/resortibojo/"
+                      target='_blank'
+                      color="black"
+                  >
+                    <Image
+                      className=" text-black"
+                      src="/instagram.svg"
+                      alt="Instagram Logo"
+                      width={20}
+                      height={20}
+                    />
+                </Link>                
+              </li>
+
+              {/* Facebook */}      
+              <li>                
+                <Link href="https://www.facebook.com/profile.php?id=100080190497521#"
+                      target='_blank'
+                  >
+                    <Image
+                      className=" text-black"
+                      src="/facebook.svg"
+                      alt="Facebook Logo"
+                      width={20}
+                      height={20}
+                    />
+                  
+                </Link>
+              </li>            
+            </ul>
+
           </ul>
         </div>
+        
+        <div>
+          {/* Legal info */}
+          <ul>
+            <li>
+              <h3 className='text-black'
+              >
+                Newsletter
+              </h3>
+            </li>
+
+            <li>
+              {/* importing text box field */}
+              <a className='text-black'>
+                text-box 
+              </a>
+            </li>
 
 
+
+          </ul>
+        </div>
+      </div>
+      
+      {/* Disclaimers */}
+      <div className="text-black text-center">
+        <ul>
+          <li>
+            <p>{new Date().getFullYear()}&copy; Copyright Bojo Resort</p>
+          </li>
+
+          {/* Credits */}
+          <li>            
+            <Link
+                className="text-black text-center hover:underline"
+                href="https://github.com/IslamiTP"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Powered By IslamiTP
+              </Link> 
+          </li>
+        </ul>
       </div>
     
     </footer>   
   );
 
 }
-
-
-
-// <p>© Copyright 2019 Bojo Resort. All Rights Reserved.</p>
-      
-//       <Link
-//         className="flex items-center gap-2 hover:underline hover:underline-offset-4
-//           text-foreground "
-//         href="https://github.com/IslamiTP"
-//         target="_blank"
-//         rel="noopener noreferrer"
-//       >
-//         Powered By IslamiTP
-//       </Link>
